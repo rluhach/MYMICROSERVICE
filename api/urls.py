8353948d3ef2ef18d1c8d456465dcc10cf020f65
Ritu Luhach <ritu.luhach@gmail.com>
2024-02-27 14:10:41 +0530

    test_commit

--- api/urls.py
+++
@@ -1,8 +0,0 @@
-# api/urls.py
-from django.urls import path
-from .views import ItemListCreateView, ItemDetailView
-
-urlpatterns = [
-    path('items/', ItemListCreateView.as_view(), name='item-list-create'),
-     path('items/<int:pk>/', ItemDetailView.as_view(), name='item-detail'),
-]
